53c8d4647351759d413b778b6c0c5bcab377c203
arman455 <snositel99@gmail.com>
2024-09-17 22:17:36 +0300

    add_server

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const moment = require("moment");
+const express = require("express");
+
+const app = express();
+const PORT = 8000;
+const HOST = 'localhost';
 
 function getDate(){
-    console.log(moment().format("YYYY/MM/DD  HH:mm:ss"));
+    const time = console.log(moment().format("YYYY/MM/DD  HH:mm:ss"));
+    return time;
 }
 
-getDate();
+app.get('/date', () => {
+    console.log(getDate());
+})
+
+app.listen(PORT, HOST, () =>{
+    console.log("server is running")
+})
+
+
+// console.log(getDate());
